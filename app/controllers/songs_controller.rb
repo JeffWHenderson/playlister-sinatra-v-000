cfd265fbb1094adaf10c7de01b1ec0c4e3b85445
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-06 23:33:10 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -42,8 +42,8 @@ flash[:message] = "Successfully created song."
     @song.update(params[:song])
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.save
-
- flash[:message] = "Successfully updated song."
+    
+    flash[:message] = "Successfully updated song."
     redirect("/songs/#{@song.slug}")
   end
 end
