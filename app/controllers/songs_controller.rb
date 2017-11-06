7ba542c78770a330a0d99ef0bba41951824a8001
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-06 23:59:29 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,6 @@ class SongsController < ApplicationController
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
-
     erb :'songs/show'
   end
 
@@ -25,9 +24,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save
-
-flash[:message] = "Successfully created song."
-
+      flash[:message] = "Successfully created song."
     redirect("/songs/#{@song.slug}")
   end
 
