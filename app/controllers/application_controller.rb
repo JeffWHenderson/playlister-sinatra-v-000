4293aa0228b71fbe2c5b990699ec621ceeaed8e3
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-06 23:13:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,7 +50,7 @@ class ApplicationController < Sinatra::Base
       @song.genre_ids = params[:genres]
       @song.save
 
-      # flash[:message] = "Successfully created song."
+      flash[:message] = "Successfully created song."
 
       redirect("/songs/#{@song.slug}")
     end
@@ -67,7 +67,7 @@ class ApplicationController < Sinatra::Base
       @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
       @song.save
 
-      # flash[:message] = "Successfully updated song."
+      flash[:message] = "Successfully updated song."
       redirect("/songs/#{@song.slug}")
     end
 
