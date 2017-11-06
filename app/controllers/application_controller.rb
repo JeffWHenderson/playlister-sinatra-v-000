59a50b705b4d133b9569f75bd7ec82e36ec3486c
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-06 23:30:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,25 +9,4 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-#========== MOVE TO GENRESCONTROLLER ============
-  get '/genres' do
-    @genres = Genre.all
-    erb :'/genres/index'
-  end
-
-  get '/genres/:slug' do
-    @genre = Genre.find_by_slug(params[:slug])
-    erb :'genres/show'
-  end
-#========== MOVE TO ARTISTSCONTROLLER ============
-  get '/artists' do
-    @artists = Artist.all
-    erb :'/artists/index'
-  end
-
-  get '/artists/:slug' do
-    @artist = Artist.find_by_slug(params[:slug])
-    erb :'artists/show'
-  end
-#========== MOVE TO SONGSCONTROLLER ============
 end
