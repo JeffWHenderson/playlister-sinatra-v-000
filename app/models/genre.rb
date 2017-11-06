81f955d6949e84e9210b10521e6cda855f049698
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-06 22:18:35 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,7 @@
 class Genre < ActiveRecord::Base
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+
   has_many :song_genres
   has_many :songs, through: :song_genre
   has_many :artists, through: :songs #only connected to genre by join table
